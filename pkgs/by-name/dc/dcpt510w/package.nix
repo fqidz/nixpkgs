e782fc60d9c54eea12bcd00482c470eb75768491
fqidz <meowthful127@gmail.com>
2024-09-07 16:49:10 +0300

    modified

--- pkgs/by-name/dc/dcpt510w/package.nix
+++ pkgs/by-name/dc/dcpt510w/package.nix
@@ -15,73 +15,72 @@
 let
   model = "dcpt510w";
 in
-{
-  driver = stdenv.mkDerivation (finalAttrs: {
-    pname = model;
-    version = "1.0.1-0";
-    src = fetchurl {
-      url = "https://download.brother.com/welcome/dlf103620/dcpt510wpdrv-${finalAttrs.version}.i386.deb";
-      hash = "sha256-hbfVL+sO9/A62q62rafzHHJ8LjuEaHNVXLXZySXiVtw=";
-    };
+stdenv.mkDerivation (finalAttrs: {
+  pname = model;
+  version = "1.0.1-0";
+  src = fetchurl {
+    url = "https://download.brother.com/welcome/dlf103620/dcpt510wpdrv-${finalAttrs.version}.i386.deb";
+    hash = "sha256-hbfVL+sO9/A62q62rafzHHJ8LjuEaHNVXLXZySXiVtw=";
+  };
 
-    nativeBuildInputs = [
-      dpkg
-      makeWrapper
-    ];
+  nativeBuildInputs = [
+    dpkg
+    makeWrapper
+  ];
 
-    buildInputs = [
-      cups
-      ghostscript
-    ];
+  buildInputs = [
+    cups
+    ghostscript
+  ];
 
-    # dontBuild = true;
+  # dontBuild = true;
 
-    unpackPhase = ''
-      dpkg-deb -x $src $out
-    '';
+  unpackPhase = ''
+    dpkg-deb -x $src $out
+  '';
 
-    # substituteInPlace $dir/lpd/filter_${model} \
-    #   --replace-fail "/usr/bin/perl" "${perl}/bin/perl" \
-    #   --replace-fail "/usr/bin/pdf2ps" "${ghostscript}/bin/pdf2ps"
+  # substituteInPlace $dir/lpd/filter_${model} \
+  #   --replace-fail "/usr/bin/perl" "${perl}/bin/perl" \
+  #   --replace-fail "/usr/bin/pdf2ps" "${ghostscript}/bin/pdf2ps"
 
-    installPhase = ''
-      dir=$out/opt/brother/Printers/${model}
+  installPhase = ''
+    dir=$out/opt/brother/Printers/${model}
 
-      # install lpr
-      patchelf --set-interpreter ${pkgsi686Linux.glibc.out}/lib/ld-linux.so.2 \
-        $dir/lpd/br${model}filter
+    # install lpr
+    patchelf --set-interpreter ${pkgsi686Linux.glibc.out}/lib/ld-linux.so.2 \
+      $dir/lpd/br${model}filter
 
-      wrapProgram $dir/inf/setupPrintcapij \
-        --prefix PATH ":" ${
-          lib.makeBinPath [
-            coreutils
-          ]
-        }
+    wrapProgram $dir/inf/setupPrintcapij \
+      --prefix PATH ":" ${
+        lib.makeBinPath [
+          coreutils
+        ]
+      }
 
-      wrapProgram $dir/lpd/filter_${model} \
-        --prefix PATH ":" ${
-          lib.makeBinPath [
-            coreutils
-            perl
-            ghostscript
-            file
-            gnused
-          ]
-        }
+    wrapProgram $dir/lpd/filter_${model} \
+      --prefix PATH ":" ${
+        lib.makeBinPath [
+          coreutils
+          perl
+          ghostscript
+          file
+          gnused
+        ]
+      }
 
 
-      echo "----------------------"
-      echo $dir
-      echo "----------------------"
-    '';
+    echo "----------------------"
+    echo $dir
+    echo "----------------------"
+  '';
 
-    meta = {
-      homepage = "http://www.brother.com/";
-      description = "Brother DCP-T510W printer driver";
-      downloadPage = "https://support.brother.com/g/b/downloadlist.aspx?c=us_ot&lang=en&prod=dcpt510w_all&os=128";
-      license = lib.licenses.unfree;
-      platforms = lib.platforms.linux;
-      maintainers = [ lib.maintainers.fqidz ];
-    };
-  });
-}
+  meta = {
+    homepage = "http://www.brother.com/";
+    description = "Brother DCP-T510W printer driver";
+    downloadPage = "https://support.brother.com/g/b/downloadlist.aspx?c=us_ot&lang=en&prod=dcpt510w_all&os=128";
+    license = lib.licenses.unfree;
+    platforms = lib.platforms.linux;
+    maintainers = [ lib.maintainers.fqidz ];
+  };
+})
+
