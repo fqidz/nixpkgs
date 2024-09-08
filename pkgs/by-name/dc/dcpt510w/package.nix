140dc0fde2f50bcd4d6e674af38c43b234b1709b
fqidz <meowthful127@gmail.com>
2024-09-09 00:34:45 +0300

    help

--- pkgs/by-name/dc/dcpt510w/package.nix
+++ pkgs/by-name/dc/dcpt510w/package.nix
@@ -17,7 +17,7 @@ let
   model = "dcpt510w";
 in
 stdenv.mkDerivation (finalAttrs: {
-  pname = model;
+  pname = "brother-${model}";
   version = "1.0.1-0";
   src = fetchurl {
     url = "https://download.brother.com/welcome/dlf103620/dcpt510wpdrv-${finalAttrs.version}.i386.deb";
@@ -49,8 +49,8 @@ stdenv.mkDerivation (finalAttrs: {
     substituteInPlace $dir/lpd/filter_${model} \
       --replace-fail "/usr/bin/perl" "${perl}/bin/perl" \
       --replace-fail "/usr/bin/pdf2ps" "${ghostscript}/bin/pdf2ps" \
-      --replace-fail "my \$PRINTER = \$0;" "my \$PRINTER = \"${model}\";" \
-      --replace-fail "my \$BR_PRT_PATH = Cwd::realpath (\$0);" "my \$BR_PRT_PATH = \"$dir\";" \
+      --replace-fail "PRINTER =~" "PRINTER = \"${model}\"; #" \
+      --replace-fail "BR_PRT_PATH =~" "BR_PRT_PATH = \"$dir\"; #" \
       --replace-fail "\`which gs\`" "\"${ghostscript}/bin/gs\""
 
     patchelf --set-interpreter "$interpreter" \
@@ -83,7 +83,6 @@ stdenv.mkDerivation (finalAttrs: {
 
     mkdir -p $out/usr/share/cups/model/Brother/
     mkdir -p $out/usr/share/ppd/Brother/
-    chmod -R a+w $out/usr/share/
 
     wrapProgram $dir/cupswrapper/cupswrapper${model} \
       --prefix PATH ":" ${
@@ -94,14 +93,23 @@ stdenv.mkDerivation (finalAttrs: {
           gnused
         ]
       }
-
-    runHook postInstall
   '';
 
   meta = {
     homepage = "http://www.brother.com/";
     description = "Brother DCP-T510W printer driver";
     downloadPage = "https://support.brother.com/g/b/downloadlist.aspx?c=us_ot&lang=en&prod=dcpt510w_all&os=128";
+    longDescription = ''
+      Install this with printing services:
+      ```
+      ...
+        services.printing.enable = true;
+        services.printing.drivers = [
+          pkgs.brother-dcpt510w
+        ];
+      ...
+      ```
+    '';
     license = lib.licenses.unfree;
     platforms = lib.platforms.linux;
     maintainers = [ lib.maintainers.fqidz ];
